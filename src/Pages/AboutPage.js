aefef35077717c1373a53512907740f73423de04
Shivansh Raheja <105767204+Shivansh-Raheja@users.noreply.github.com>
2024-12-30 18:53:17 +0530

    addition

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { FaProjectDiagram, FaUsers, FaCalendarAlt ,FaCertificate} from 'react-icons/fa';
 import './AboutPage.css';
 
 const AboutPage = () => {
+    useEffect(() => {
+        const counters = document.querySelectorAll('.counter');
+        const speed = 200;
+    
+        counters.forEach(counter => {
+          const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const current = +counter.innerText;
+    
+            const increment = target / speed;
+            
+            if (current < target) {
+              counter.innerText = Math.ceil(current + increment);
+              setTimeout(updateCount, 1);
+            } else {
+              counter.innerText = target;
+            }
+          };
+          
+          updateCount();
+        });
+      }, []);
   return (
     <div className="about-page">
       {/* About Us Section */}
@@ -44,6 +67,58 @@ const AboutPage = () => {
           </div>
         </div>
       </section>
+
+      <section className="statistics-section">
+    <div className="statistics-heading">
+        <h2>Get to Know Us More</h2>
+    </div>
+    <div className="statistics-container">
+        {/* Column 1: Projects Completed */}
+        <div className="statistics-column">
+            <div className="statistics-icon">
+                <FaProjectDiagram />
+            </div>
+            <div className="statistics-content">
+                <h3>Projects Completed</h3>
+                <p className="counter" data-target="244">0</p>
+            </div>
+        </div>
+
+        {/* Column 2: Number of Employees */}
+        <div className="statistics-column">
+            <div className="statistics-icon">
+                <FaUsers />
+            </div>
+            <div className="statistics-content">
+                <h3>No.of Employees</h3>
+                <p className="counter" data-target="136">0</p>
+            </div>
+        </div>
+
+        {/* Column 3: Years of Experience */}
+        <div className="statistics-column">
+            <div className="statistics-icon">
+                <FaCalendarAlt />
+            </div>
+            <div className="statistics-content">
+                <h3>Years of Experience</h3>
+                <p className="counter" data-target="27">0</p>
+            </div>
+        </div>
+
+        {/* Column 4: Certified By */}
+        <div className="statistics-column">
+            <div className="statistics-icon">
+                <FaCertificate /> {/* Add an icon for certification */}
+            </div>
+            <div className="statistics-content">
+                <h3>Our Happy clients</h3>
+                <p className="counter" data-target="50">0</p> {/* Example of certification number */}
+            </div>
+        </div>
+    </div>
+</section>
+
     </div>
   );
 };
