f07c529b0666041e9ae4343b6645447059082ce6
Shivansh Raheja <105767204+Shivansh-Raheja@users.noreply.github.com>
2025-01-09 18:37:45 +0530

    minor changes

--- src/Pages/Health.js
+++ src/Pages/Health.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useEffect } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './HealthSafety.css'; // Custom CSS for styling
 
 const HealthSafety = () => {
+  useEffect(() => {
+          window.scrollTo(0, 0);
+        }, []);
   const sliderSettings = {
     dots: false,
     arrows: false,
