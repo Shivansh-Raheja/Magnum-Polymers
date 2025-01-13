3ccdea8ea4bf9f67bbd32f14222cc83ecac159a0
Shivansh Raheja <105767204+Shivansh-Raheja@users.noreply.github.com>
2025-01-13 17:41:40 +0530

    kaizen

--- src/Pages/InnovationAndKaizen.js
+++ src/Pages/InnovationAndKaizen.js
@@ -14,7 +14,7 @@ const InnovationAndKaizen = () => {
           <Col md={6} className="text-section">
             <h2 className="section-heading">Innovation and Kaizen</h2>
             <p className="section-paragraph">
-              Kaizen, a Japanese term meaning "change for the better" or "continuous improvement," is a fundamental philosophy in manufacturing. It emphasizes the importance of making small, incremental improvements in processes, products, or services to enhance efficiency and quality. For manufacturing firms, adhering to Kaizen principles fosters a culture of innovation, reduces waste, improves productivity, and leads to higher customer satisfaction.
+              MAGNUM actively embraces Kaizen, a Japanese term meaning "change for the better" or "continuous improvement," is a fundamental philosophy in manufacturing. It emphasizes the importance of making small, incremental improvements in processes, products, or services to enhance efficiency and quality. For manufacturing firms, adhering to Kaizen principles fosters a culture of innovation, reduces waste, improves productivity, and leads to higher customer satisfaction.
             </p>
           </Col>
           <Col md={6} className="image-section">
